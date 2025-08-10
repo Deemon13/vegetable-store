2c9e2f48f9c1d0a2afdfc04425c8714763aa3686
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-08-10 21:31:14 +0300

    refactor: создание отдельного компонента кнопки Корзина

---
+++ src/modules/UI/CartButton/CartButton.tsx
@@ -0,0 +1,35 @@
+import "@mantine/core/styles.css";
+
+import { Popover, Button } from "@mantine/core";
+import { IconShoppingCart } from "@tabler/icons-react";
+
+import styles from "./CartButton.module.css";
+
+interface CartButtonProps {
+  cart: {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    category?: string;
+    amount: number;
+  }[];
+}
+
+export const CartButton = ({ cart }: CartButtonProps) => {
+  return (
+    <Popover.Target>
+      <Button
+        variant="filled"
+        color="#54b46a"
+        rightSection={<IconShoppingCart size={20} />}
+        className={styles["btn-cart"]}
+      >
+        {cart.length > 0 && (
+          <span className={styles["veg-amount"]}>{cart.length}</span>
+        )}
+        Cart
+      </Button>
+    </Popover.Target>
+  );
+};
