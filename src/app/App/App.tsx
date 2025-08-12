3f017632194c2826a0ab47f0a3d1f8afe595d239
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-08-12 23:10:30 +0300

    refactor: добавляет компонент VegetablesList

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -2,19 +2,17 @@ import ky from "ky";
 
 import "@mantine/core/styles.css";
 
-import { useEffect, useState, type MouseEvent } from "react";
+import { useEffect, useState } from "react";
 import {
   MantineProvider,
   createTheme,
   type MantineColorsTuple,
   AppShell,
-  SimpleGrid,
 } from "@mantine/core";
 
 import "./App.css";
 
-import { Header } from "../../pages";
-import { VegetableCard } from "../../modules/components";
+import { Header, VegetablesList } from "../../pages";
 import { CatalogTitle } from "../../modules/UI";
 
 const myColor: MantineColorsTuple = [
@@ -65,82 +63,6 @@ export const App = () => {
     getVegetables();
   }, []);
 
-  function handleIncreaseAmount(
-    evt: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
-  ) {
-    const vegetablesWithIncreaseAmount = vegetables.map((item) => {
-      if (item.id !== Number((evt.target as HTMLElement).offsetParent?.id)) {
-        return item;
-      } else {
-        item.amount += 1;
-        return item;
-      }
-    });
-
-    setVegetables([...vegetablesWithIncreaseAmount]);
-  }
-
-  function handleDecreaseAmount(
-    evt: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
-  ) {
-    const vegetablesWithIncreaseAmount = vegetables.map((item) => {
-      if (item.id !== Number((evt.target as HTMLElement).offsetParent?.id)) {
-        return item;
-      } else {
-        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        item.amount <= 0 ? (item.amount = 0) : (item.amount -= 1);
-        return item;
-      }
-    });
-
-    setVegetables([...vegetablesWithIncreaseAmount]);
-  }
-
-  function handleAddToCart(
-    evt: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
-  ) {
-    let newCart = [];
-
-    const vegetableToCartId = Number(
-      (evt.target as HTMLElement).offsetParent?.id
-    );
-
-    const vegetableToCart: VegetableType | undefined = vegetables.find(
-      (item) => item.id === vegetableToCartId
-    );
-
-    if (!vegetableToCart || vegetableToCart.amount === 0) {
-      return;
-    }
-
-    if (cart.some((item) => item.id === vegetableToCartId)) {
-      const vegetableExist = cart.find((item) => item.id === vegetableToCartId);
-      const amountToAdd = vegetableToCart.amount;
-
-      if (vegetableExist) {
-        vegetableExist.amount += amountToAdd;
-      }
-
-      newCart = [...cart];
-    } else {
-      newCart = [...cart, vegetableToCart];
-    }
-
-    setCart([...newCart]);
-
-    const vegetablesWithAmount = vegetables.map((item) => {
-      if (item.id !== vegetableToCartId) {
-        return item;
-      } else {
-        const newItem = { ...item };
-        newItem.amount = 1;
-        return newItem;
-      }
-    });
-
-    setVegetables([...vegetablesWithAmount]);
-  }
-
   return (
     <MantineProvider theme={theme}>
       <AppShell header={{ height: 60 }}>
@@ -155,28 +77,12 @@ export const App = () => {
         <AppShell.Main className="main">
           <CatalogTitle title="Catalog" />
 
-          <SimpleGrid
-            cols={{ base: 1, sm: 2, lg: 4 }}
-            spacing={{ base: 10, sm: "md" }}
-            verticalSpacing={{ base: "md", sm: "md" }}
-          >
-            {vegetables.map(({ id, image, name, price, amount }) => {
-              return (
-                <VegetableCard
-                  key={id}
-                  id={id}
-                  image={image}
-                  name={name}
-                  price={price}
-                  amount={amount}
-                  increaseAmount={(evt) => handleIncreaseAmount(evt)}
-                  decreaseAmount={(evt) => handleDecreaseAmount(evt)}
-                  category={""}
-                  addToCart={(evt) => handleAddToCart(evt)}
-                />
-              );
-            })}
-          </SimpleGrid>
+          <VegetablesList
+            vegetables={vegetables}
+            setVegetables={setVegetables}
+            cart={cart}
+            setCart={setCart}
+          />
         </AppShell.Main>
       </AppShell>
     </MantineProvider>
